c940e7ded33f366872809aac523b157d2cce5e93
codebatine <codebatine@proton.me>
2024-05-01 18:15:45 +0200

    Kickoff :coffee:

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 export const Home = () => {
   return (
     <div>---Home---
-      <div>CONTENT</div>
+          <div>---Home---
+      <div className="container">
+        <h1 className="coffee_name">Pure Coffee</h1>
+        <div className="cup">
+          <div className="filling reset">
+            <div className="coffee">100% organic, small farmed beans!</div>
+          </div>
+          <div className="plate"></div>
+        </div>
+      </div>
+    </div>
       <div>CONTENT</div>
       <div>CONTENT</div>
       <div>CONTENT</div>
