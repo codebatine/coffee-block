1c3adee55590124419736b948c3bbe2e9a0bb187
codebatine <codebatine@proton.me>
2024-05-02 16:18:37 +0200

    footer start :foot:

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,5 +1,36 @@
+
+import signalImg from '../../content/img/signal.png';
+import telegramImg from '../../content/img/telegram.png';
+
 export const Footer = () => {
   return (
-    <div className="footer">---Footer---</div>
+    <footer className="footer">
+
+      <div className="copyright">
+        <p>© 2024 Coffee Block.</p>
+      </div>
+      <div className="social-links">
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={signalImg}
+            alt="Signal"
+          />
+        </a>
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={telegramImg}
+            alt="Telegram"
+          />
+        </a>
+      </div>
+    </footer>
   )
 }
