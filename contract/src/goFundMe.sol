f18d5b52509bd65685a0a33613195ebf11c57802
Reblixt <carl@klofverskjold.se>
2024-05-13 12:56:25 +0200

    A basic FundMe contract

--- contract/src/goFundMe.sol
+++ contract/src/goFundMe.sol
@@ -16,6 +16,12 @@
 
 pragma solidity ^0.8.25;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+import {Constants} from "./constants/Constants.c.sol";
+
+error NotOwner();
+
 contract goFundMe {
     address public owner;
     uint256 public goal;
@@ -23,20 +29,58 @@ contract goFundMe {
     uint256 public balance;
     mapping(address => uint256) public contributions;
 
-    constructor() {
-        owner = msg.sender;
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    address public immutable i_owner;
+    AggregatorV3Interface private s_priceFeed;
+
+    constructor(address priceFeed) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
-    function deposit() public payable {
-        // implement a function to send data/message to the goFundMe contract owner.
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= Constants.MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
-    function withdraw() public onlyOwner {}
-
-    function refund() public {}
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
+        if (msg.sender != i_owner) revert NotOwner();
         _;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
     }
 }
