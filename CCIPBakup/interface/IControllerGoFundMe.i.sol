433074c44bee8679c12dcac2305d909b62edf7de
Reblixt <carl@klofverskjold.se>
2024-05-27 17:56:28 +0200

    removed old stuff

--- CCIPBakup/interface/IControllerGoFundMe.i.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.18;
-
-interface IControllerGoFundMe {
-    function getProjectList() external view returns (address[] memory);
-
-    function getProject(uint256 _index) external view returns (address);
-}
