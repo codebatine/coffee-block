211798b2a3f48ba3d3c8457035fbd021d8661e8d
Rasmus <rasmus.wersall@gmail.com>
2024-05-31 13:21:56 +0200

    :sparkles: update

--- frontend/src/models/Application.jsx
+++ frontend/src/models/Application.jsx
@@ -1,15 +1,25 @@
 export default class Application {
-  constructor (company, amount, index, owner, area, reason, time, name, email, lastUpdate, published){
+  constructor (company, amount, index, owner, project, area, reason, time, name, email, lastUpdate, published){
     this.company = company || "company";
     this.amount = amount || "amount";
     this.index = index || "index"
     this.owner = owner || "owner"
+    this.project = project || "project"
     this.area = area || "area";
     this.reason = reason || "reason";
     this.time = time || "time";
     this.name = name || "name";
     this.email = email || "email";
-    this.lastUpdate = lastUpdate;
+    this.lastUpdate = lastUpdate || new Date();
     this.published = published || "no";
+    this.image = {src: this.src(), alt: ""}
+  }
+
+  src() {
+
+
+    const url = "/content/img/coffee-1.jpg"
+
+    return url;
   }
 }
