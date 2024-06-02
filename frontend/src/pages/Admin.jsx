86fbf9e186d03278fb2c0ab1ee6d264811a7a5e6
codebatine <codebatine@proton.me>
2024-06-02 19:17:33 +0200

    admin css

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,17 +1,14 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export const Admin = () => {
 
-  const [applications, setApplications] = useState([])
+  const [applications, setApplications] = useState([]);
 
   const handlePublish = async (id, e) => {
     e.preventDefault();
     try {
-      // Update the published status in the backend
       await axios.patch(`http://localhost:3001/api/v1/applications/publish/${id}`, { published: "yes" });
-
-      // Update the published status in the local state
       setApplications((prevApplications) =>
         prevApplications.map((application) =>
           application.id === id ? { ...application, published: "yes" } : application
@@ -25,10 +22,7 @@ export const Admin = () => {
   const handleUnPublish = async (id, e) => {
     e.preventDefault();
     try {
-      // Update the published status in the backend
       await axios.patch(`http://localhost:3001/api/v1/applications/publish/${id}`, { published: "no" });
-
-      // Update the published status in the local state
       setApplications((prevApplications) =>
         prevApplications.map((application) =>
           application.id === id ? { ...application, published: "no" } : application
@@ -39,46 +33,50 @@ export const Admin = () => {
     }
   };
 
-  // const handleClick = async () => {
-    useEffect(()=> {
-      const getApplications = async () => {
-        try {
-          const response = await axios.get('http://localhost:3001/api/v1/applications/')
-          setApplications(response.data)
-        } catch (error) {
-          console.error('There was an error listing the applications!', error);
-        }
+  useEffect(() => {
+    const getApplications = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/v1/applications/');
+        setApplications(response.data);
+      } catch (error) {
+        console.error('There was an error listing the applications!', error);
       }
-
-      getApplications();
-  }, [])
-
-  
+    };
+    getApplications();
+  }, []);
 
   return (
-    <div className="admin-wrapper">
+    <div className="content-container">
       <h2>Admin</h2>
-
       <div className="application-admin-wrapper">
-        <h3>Applications</h3>
-        {/* <button onClick={handleClick}>Retrieve applications</button> */}
-        {applications.length > 0 ? 
-        <section>{applications.map((application) => 
-        <div key={application.id} className="application-admin-display">
-          <div>{application.company || "company missing"}</div>
-          <div>{application.amount || "amount missing"}</div>
-          <div>{application.area || "area missing"}</div>
-          <div>{application.reason || "reason missing"}</div>
-          <div>{application.time || "time missing"}</div>
-          <div>{application.date || "date missing"}</div>
-          
-          <div className="admin-publish-buttons">{application.published === "no" ? <button className="button-pub" onClick={(e) => handlePublish(application.id, e)}>UNPUBLISHED</button> : <button className="button-unpub" onClick={(e) => handleUnPublish(application.id, e)} >PUBLISHED</button>}</div>
-        </div>
-      )}</section>
-        :
-        <div>Inga applikationer funna</div>
-        }
+        {applications.length > 0 ? (
+          <section>
+            {applications.map((application) => (
+              <div key={application.id} className="application-admin-display">
+                <div className="application-detail" style={{fontWeight: 'bold'}}>{application.company || "company missing"}</div>
+                <div className="application-detail">{`${application.amount || "amount missing"} USDC`}</div>
+                <div className="application-detail">{application.area || "area missing"}</div>
+                <div className="application-detail">{application.reason || "reason missing"}</div>
+                <div className="application-detail">{application.time || "time missing"}</div>
+                <div className="application-detail">{application.date || "date missing"}</div>
+                <div className="admin-publish-buttons">
+                  {application.published === "no" ? (
+                    <button className="application-button" onClick={(e) => handlePublish(application.id, e)}>
+                      Unlisted
+                    </button>
+                  ) : (
+                    <button className="application-button" onClick={(e) => handleUnPublish(application.id, e)}>
+                      Published
+                    </button>
+                  )}
+                </div>
+              </div>
+            ))}
+          </section>
+        ) : (
+          <div>Inga applikationer funna</div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
