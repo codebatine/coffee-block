177241f4c05f893393150fec95d7fc17732b892e
codebatine <codebatine@proton.me>
2024-06-02 20:06:30 +0200

    about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,5 @@
+import img1 from '../content/img/coffee-8.jpg';
+
 export const About = () => {
   return (
     <div className="content-container">
@@ -8,12 +10,7 @@ export const About = () => {
         <button className="application-button">GitHub Repo</button>
         </a>
         <p>We would like to improve this further and are open for suggestions and disucssions. Here's our <a href="https://t.me/+yHxclZ9JCyQ1ZjFk" target="_blank" rel="noopener noreferrer">Coffee Block Telegram group. ☕</a></p>
-        <h2>The Team</h2>
-        <ul>
-          <li><a href="https://github.com/Reblixt" target="_blank" rel="noopener noreferrer">Carl Klöfverskjöld</a></li>
-          <li><a href="https://github.com/devmus" target="_blank" rel="noopener noreferrer">Rasmus Wersäll</a></li>
-          <li><a href="https://github.com/codebatine" target="_blank" rel="noopener noreferrer">Sanjin Đumišić</a></li>
-        </ul>
+        <img className="responsive-img" src={img1} alt="Coffee." />
       </div>
   )
 }
